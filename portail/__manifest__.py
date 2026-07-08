c2615999f67fef3522075159e7d4ff44264574c1
kiemdegerald <kiemdegeralde@gmail.com>
2026-07-08 17:32:00 +0000

    Initialisation du module Portail Employé (structure de base)

---
+++ portail/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Portail Employé (Self-Service)",
+    "summary": "Espace self-service RH pour les employés via des utilisateurs Portail (gratuits).",
+    "author": "LICELI Technologies",
+    "website": "https://www.liceli-technologies.com",
+    "category": "Human Resources",
+    "version": "17.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["portal", "website", "hr_payroll", "hr_holidays"],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
