c04c5988cd1d8f2de4c9d490fd33ef3d19a376e4
kiemdegerald <kiemdegeralde@gmail.com>
2026-08-03 10:15:00 +0000

    Initiative des demandes paramétrable : employé et/ou chef de service (portail)

--- gestion_mission/models/res_config_settings.py
+++ gestion_mission/models/res_config_settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
@@ -17,3 +17,37 @@ class ResConfigSettings(models.TransientModel):
         related="company_id.gm_dg_name", readonly=False,
         help="Propre à la société sélectionnée : imprimé dans le bloc "
              "signature des documents de mission.")
+
+    # Qui peut INITIER une demande depuis le portail. Stockage explicite
+    # "1"/"0" (pas de config_parameter automatique : décocher supprimerait
+    # le paramètre et le réglage reviendrait silencieusement à "autorisé").
+    gm_portal_employee_can_create = fields.Boolean(
+        string="Les employés créent leurs demandes (portail)",
+        help="L'employé peut créer et soumettre ses propres demandes de "
+             "mission depuis le portail. Décoché : il ne fait que suivre "
+             "les demandes créées pour lui.")
+    gm_portal_chef_can_create = fields.Boolean(
+        string="Les chefs créent pour leurs collaborateurs (portail)",
+        help="Un supérieur hiérarchique peut créer une demande au nom d'un "
+             "de ses collaborateurs directs depuis le portail (brouillon à "
+             "compléter par l'agent, ou soumission directe).")
+
+    @api.model
+    def get_values(self):
+        res = super().get_values()
+        icp = self.env["ir.config_parameter"].sudo()
+        res.update(
+            gm_portal_employee_can_create=icp.get_param(
+                "gm.portal_employee_can_create", "1") == "1",
+            gm_portal_chef_can_create=icp.get_param(
+                "gm.portal_chef_can_create", "1") == "1",
+        )
+        return res
+
+    def set_values(self):
+        super().set_values()
+        icp = self.env["ir.config_parameter"].sudo()
+        icp.set_param("gm.portal_employee_can_create",
+                      "1" if self.gm_portal_employee_can_create else "0")
+        icp.set_param("gm.portal_chef_can_create",
+                      "1" if self.gm_portal_chef_can_create else "0")
